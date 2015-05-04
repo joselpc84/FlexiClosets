29f5ae8262defc703e86d7e20b7ef75b4a484baa
Luis Alejandro Vieira <lavz24@gmail.com>
2015-05-04 08:46:45 -0430

    Added move object after its dragged

--- Assets/FlexiCloset/Scripts/GUI/GUI_ItemController.cs
+++ Assets/FlexiCloset/Scripts/GUI/GUI_ItemController.cs
@@ -3,60 +3,68 @@ using System.Collections;
 
 public class GUI_ItemController : PersistentSingleton<GUI_ItemController>
 {
-    public Item item;
+	public Item item;
 
-    public void RotaeLeft()
-    {
-        if (item != null)
-        {
-            StopCoroutine("ResetClick");
-            StartCoroutine("ResetClick", false);
-            item.Rotate(-1);
-        }
-    }
+	public void RotaeLeft ()
+	{
+		if (item != null) {
+			StopCoroutine ("ResetClick");
+			StartCoroutine ("ResetClick", false);
+			item.Rotate (-1);
+		}
+	}
 
-    public void RotaeRigth()
-    {
-        if (item != null)
-        {
-            StopCoroutine("ResetClick");
-            StartCoroutine("ResetClick", false);
-            item.Rotate(1);
-        }
-    }
+	public void RotaeRigth ()
+	{
+		if (item != null) {
+			StopCoroutine ("ResetClick");
+			StartCoroutine ("ResetClick", false);
+			item.Rotate (1);
+		}
+	}
 
-    public void Remove()
-    {
-        if (item != null)
-        {
-            item.Remove();
-        }
-    }
+	public void Move ()
+	{
+		if (item != null) {
+			//StopCoroutine ("ResetClick");
+			//StartCoroutine ("ResetClick", false);
+			item.Move ();
+		}
+	}
 
-    public void SetNoInput()
-    {
-        StopCoroutine("ResetClick");
-        ManagerInputItem.Instance.isClickOnGUI = true;
+	public void Remove ()
+	{
+		if (item != null) {
+			StopCoroutine ("ResetClick");
+			StartCoroutine ("ResetClick", false);
+			item.Remove ();
+		}
+	}
 
-    }
+	public void SetNoInput ()
+	{
+		StopCoroutine ("ResetClick");
+		ManagerInputItem.Instance.isClickOnGUI = true;
 
-    IEnumerator ResetClick(bool value)
-    {
+	}
 
-        yield return new WaitForSeconds(0.2f);
-        ManagerInputItem.Instance.isClickOnGUI = value;
+	IEnumerator ResetClick (bool value)
+	{
 
-    }
+		yield return new WaitForSeconds (0.2f);
+		ManagerInputItem.Instance.isClickOnGUI = value;
 
-    public void ActivateGUI(Item item)
-    {
-        this.item = item;
-    }
+	}
 
-    public void DeActivateGUI()
-    {
-        this.item = null;
-    }
+	public void ActivateGUI (Item item)
+	{
+		this.item = item;
+	}
+
+	public void DeActivateGUI ()
+	{
+		this.item = null;
+	}
         
 
 }
--- Assets/FlexiCloset/Scripts/ManagerItemDrag.cs
+++ Assets/FlexiCloset/Scripts/ManagerItemDrag.cs
@@ -8,83 +8,95 @@ using System.Collections;
 public class ManagerItemDrag : PersistentSingleton<ManagerItemDrag>
 {
 
-    protected Item itemSpawned = null;
-    public float OffSetY = 0.01f;
+	protected Item itemSpawned = null;
+	public float OffSetY = 0.01f;
+	bool NotSpawned = false;
 
-    public void OnDrag(Item item)
-    {
-        ManagerInputItem.Instance.isClickOnGUI = true;
+	public void OnDrag (Item item)
+	{
+		ManagerInputItem.Instance.isClickOnGUI = true;
+		NotSpawned = false;
+		itemSpawned = item.Spawn ();
+		if (ManagerMouseControl.Instance.CurrentMousePos ().HasValue) {
+			itemSpawned.SetPos (ManagerMouseControl.Instance.CurrentMousePos ().Value);
+		} else {
+			itemSpawned.transform.position = Camera.main.ScreenToWorldPoint (Input.mousePosition);
+		}
+		itemSpawned.OnDrag ();
+	}
 
-        itemSpawned = item.Spawn();
-        if (ManagerMouseControl.Instance.CurrentMousePos().HasValue)
-        {
-            itemSpawned.SetPos(ManagerMouseControl.Instance.CurrentMousePos().Value);
-        }
-        else
-        {
-            itemSpawned.transform.position = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        }
-        itemSpawned.OnDrag();
-    }
+	public void OnDragWhitoutSpawn (Item item)
+	{
+		ManagerInputItem.Instance.isClickOnGUI = true;
 
-    public void OnDrop()
-    {
-        if (ManagerMouseControl.Instance.CurrentMousePos().HasValue)
-        {
-            if (ManagerItemGrid.Instance.isEmptySpot(ManagerMouseControl.Instance.CurrentMousePos().Value, itemSpawned))
-            {
-                itemSpawned.OnDrop();
+		itemSpawned = item;
+		StopCoroutine ("WaitSetNotSpawned");
+		StartCoroutine ("WaitSetNotSpawned", true);
+		if (ManagerMouseControl.Instance.CurrentMousePos ().HasValue) {
+			itemSpawned.SetPos (ManagerMouseControl.Instance.CurrentMousePos ().Value);
+		} else {
+			itemSpawned.transform.position = Camera.main.ScreenToWorldPoint (Input.mousePosition);
+		}
+		itemSpawned.OnDrag ();
+	}
 
-                ManagerItemGrid.Instance.AddItem(ManagerMouseControl.Instance.CurrentMousePos().Value, itemSpawned);
+	public void OnDrop ()
+	{
+		if (ManagerMouseControl.Instance.CurrentMousePos ().HasValue) {
+			if (ManagerItemGrid.Instance.isEmptySpot (ManagerMouseControl.Instance.CurrentMousePos ().Value, itemSpawned)) {
+				itemSpawned.OnDrop ();
 
-            }
-            else
-            {
-                itemSpawned.Recycle();
+				ManagerItemGrid.Instance.AddItem (ManagerMouseControl.Instance.CurrentMousePos ().Value, itemSpawned);
 
-            }
-        }
-        else
-        {
-            itemSpawned.Recycle();
+			} else {
+				itemSpawned.Recycle ();
 
-        }
+			}
+		} else {
+			itemSpawned.Recycle ();
 
-        itemSpawned = null;
-        StopCoroutine("ResetClick");
-        StartCoroutine("ResetClick", false);
+		}
+		NotSpawned = false;
+		itemSpawned = null;
+		StopCoroutine ("ResetClick");
+		StartCoroutine ("ResetClick", false);
 
-    }
+	}
 
-    IEnumerator ResetClick(bool value)
-    {
+	IEnumerator WaitSetNotSpawned (bool value)
+	{
 
-        yield return new WaitForSeconds(0.2f);
-        ManagerInputItem.Instance.isClickOnGUI = value;
-    }
+		yield return new WaitForSeconds (0.2f);
+		NotSpawned = value;
+	}
 
-    void Update()
-    {
+	IEnumerator ResetClick (bool value)
+	{
 
-        if (itemSpawned != null)
-        {
-            if (ManagerMouseControl.Instance.CurrentMousePos().HasValue)
-            {
+		yield return new WaitForSeconds (0.2f);
+		ManagerInputItem.Instance.isClickOnGUI = value;
+	}
+
+	void Update ()
+	{
+
+		if (itemSpawned != null) {
+			if (ManagerMouseControl.Instance.CurrentMousePos ().HasValue) {
                
-                itemSpawned.SetPos(ManagerMouseControl.Instance.CurrentMousePos().Value);
-                if (ManagerItemGrid.Instance.isEmptySpot(ManagerMouseControl.Instance.CurrentMousePos().Value, itemSpawned))
-                {
-                    itemSpawned.ChangueColorPlane(Color.green);
-                    //Aqui CAmbio las texturas etc, etc
-                }
-                else
-                {
-                    itemSpawned.transform.position = itemSpawned.transform.position + Vector3.up * OffSetY;
-                    itemSpawned.ChangueColorPlane(Color.red);
+				itemSpawned.SetPos (ManagerMouseControl.Instance.CurrentMousePos ().Value);
+				if (ManagerItemGrid.Instance.isEmptySpot (ManagerMouseControl.Instance.CurrentMousePos ().Value, itemSpawned)) {
+					itemSpawned.ChangueColorPlane (Color.green);
+					//Aqui CAmbio las texturas etc, etc
+				} else {
+					itemSpawned.transform.position = itemSpawned.transform.position + Vector3.up * OffSetY;
+					itemSpawned.ChangueColorPlane (Color.red);
 
+				}
 
-                }
-            }
-        }
-    }
+				if (NotSpawned && Input.GetMouseButtonDown (0)) {
+					OnDrop ();
+				}
+			}
+		}
+	}
 }
